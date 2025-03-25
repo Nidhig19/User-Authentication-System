7243d931f7c249bd608a3d1864031ff31244e959
Nidhig19 <115439813+Nidhig19@users.noreply.github.com>
2025-03-25 17:24:14 +0530

    Add files via upload
    Files for email verification , incomplete till logging in to dashboard

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+$page_title = "Home page";
+include('includes/header.php'); 
+include('includes/navbar.php'); 
+?>
+    <div class="py-5">
+        <div class="container">
+            <div class="col-md-12 text-center" >
+                <h2>Login and Registration using Email</h2>
+                <h4>With Email Verification</h4>
+            </div>
+        </div>
+    </div>
+<?php include('includes/footer.php'); ?>
